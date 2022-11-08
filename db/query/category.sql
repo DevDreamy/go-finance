8fe58baf3fb0238d18f93728754fda25aa8d9693
Leo <leozhaunr@yahoo.com.br>
2022-11-08 18:56:36 -0300

    style: add line breaks

--- db/query/category.sql
+++ db/query/category.sql
@@ -9,13 +9,20 @@ INSERT INTO categories (
 ) RETURNING *;
 
 -- name: GetCategory :one
-SELECT * FROM categories WHERE id = $1 LIMIT 1;
+SELECT * FROM categories
+WHERE id = $1 LIMIT 1;
 
 -- name: GetCategories :many
-SELECT * FROM categories WHERE user_id = $1 AND type = $2 AND title LIKE $3 AND description LIKE $4;
+SELECT * FROM categories
+WHERE user_id = $1 AND type = $2
+AND title LIKE $3 AND description LIKE $4;
 
 -- name: UpdateCategory :one
-UPDATE categories SET title = $2, description = $3 WHERE id = $1 RETURNING *;
+UPDATE categories
+SET title = $2, description = $3
+WHERE id = $1
+RETURNING *;
 
 -- name: DeleteCategory :exec
-DELETE FROM categories WHERE id = $1 RETURNING *;
+DELETE FROM categories
+WHERE id = $1 RETURNING *;
